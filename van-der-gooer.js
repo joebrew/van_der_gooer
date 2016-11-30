3f0aabfe0f3afeafc6b606faa40fee532d2cdc92
Andrzej Jarzyna <andrew.jarzyna@gmail.com>
2016-11-30 12:56:14 +0200

    WiP of DBSCAN in js

--- van-der-gooer.js
+++ van-der-gooer.js
@@ -14,11 +14,9 @@ function normal_array( mean, stddev, size ){
 }
 
 var x0 = normal_array(2, 0.45, 300);
-console.log(x0);
 var y0 = normal_array(2, 0.45, 300);
 
 var x1 = normal_array(6, 0.4, 200);
-console.log(x1);
 var y1 = normal_array(6, 0.4, 200)
 
 var x2 = normal_array(4, 0.3, 200);
@@ -28,6 +26,7 @@ var data = [
     {    
         x: x0,
         y: y0,
+        name: 'cluster1',
         mode: 'markers',
         marker: {
           color: 'rgb(0,0,0)'
@@ -35,6 +34,7 @@ var data = [
     }, {
         x: x1,
         y: y1,
+        name: 'cluster2',
         mode: 'markers'   ,
         marker: {
           color: 'rgb(0,0,0)'
@@ -42,6 +42,7 @@ var data = [
     }, {
         x: x2,
         y: y2,
+        name: 'cluster3',
         mode: 'markers',
         marker: {
           color: 'rgb(0,0,0)'
@@ -49,6 +50,7 @@ var data = [
     }, {
         x: x1,
         y: y0,
+        name: 'cluster4',
         mode: 'markers',
         marker: {
           color: 'rgb(0,0,0)'
@@ -63,3 +65,46 @@ var layout = {
 }
 
 Plotly.newPlot('myDiv', data, layout);
+
+var all_x = x0.concat(x1);
+all_x.concat(x2);
+var all_y = y0.concat(y1);
+all_y.concat(y2);
+console.log(all_x);
+
+// DBSCAN implementation
+function dbscan(d, eps, minPts) {
+  var c=[];
+  var visited = [];
+  var noise = [];
+  //var dPrime = d;
+  for(var i=0;i<d.length;d++) {   
+    var neighbourPoints;
+    visited.push(d[i]); 
+    neighbourPoints.push(regionQuery(d, d[i], eps));
+    if(neighbourPoints.length<minPts) {
+
+    }
+  }
+}
+
+function regionQuery(d, point, eps) {
+  var neighbourPoints = [];
+  for(var i=0;i<d.length;i++) {
+    if (pointDistance(point, d[i]) <= eps ) {
+      neighbourPoints.push(d[i]);
+    }
+  }
+  return neighbourPoints;
+}
+
+function pointDistance(point1, point2) {
+  var x1 = point1[0];
+  var y1 = point1[1];
+  var x2 = point2[0];
+  var y2 = point2[1];
+
+  var a = Math.abs(x1-x2);
+  var b = Math.abs(y1-y2);
+  return Math.sqrt(a*a + b*b);
+}
