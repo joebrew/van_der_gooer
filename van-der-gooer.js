aaf9c2db31ae433b17837925b5e357f2a8a275f5
jerzyn <andrew.jarzyna@gmail.com>
2017-01-15 18:42:01 +0200

    Finish DBSCAN draft. No testing

--- van-der-gooer.js
+++ van-der-gooer.js
@@ -72,28 +72,63 @@ var all_y = y0.concat(y1);
 all_y.concat(y2);
 console.log(all_x);
 
+// ----------------------------------------------------------------------------------------------------
+
+d3.csv("./data/testing_data_sets/data_set_1.csv", function(data) {
+  console.log(data);
+});
+
+// ----------------------------------------------------------------------------------------------------
+
 // DBSCAN implementation
+
+// DBSCAN requires two parameters: eps and the minimum number of points required to form a dense region[a] (minPts). 
+// It starts with an arbitrary starting point that has not been visited. 
+// This point's eps-neighborhood is retrieved, and if it contains sufficiently many points, a cluster is started. 
+// Otherwise, the point is labeled as noise. Note that this point might later be found in a sufficiently sized eps-environment of a different point and hence be made part of a cluster.
+// If a point is found to be a dense part of a cluster, its eps-neighborhood is also part of that cluster. 
+// Hence, all points that are found within the eps-neighborhood are added, as is their own eps-neighborhood when they are also dense. 
+// This process continues until the density-connected cluster is completely found. Then, a new unvisited point is retrieved and processed, leading to the discovery of a further cluster or noise.
+
+// We assume that the structure of the set is as follows:
+// var set = [
+//   {
+//     x: value,
+//     y: value,
+//     label: value,
+//     true_label: value
+//   }
+// ]
 function dbscan(set, eps, minPts) {
   var c=[];
   var visited = [];
   var noise = [];
   //var dPrime = d;
   for(var i=0;i<set.length;d++) {   
-    var neighbourPoints;
+    var neighbourPoints = [];
     visited.push(set[i]); 
-    neighbourPoints.push(regionQuery(set, set[i], eps)); // review this code
+    neighbourPoints.concat(regionQuery(set, i, eps)); // review this code
     if(neighbourPoints.length<minPts) {
       noise.push(set[i]);
     } else {
-      
+      var clusterId = c.length;
+      c.push([]);
+      c[clusterId].push(set[i]);
+      var expansion = expandCluster(set[i], neighbourPoints, c[clusterId], eps, minPts, visited);
+      visited = expansion.visited_points;
+      neighbourPoints = expansion.neighbour_points;
+      c = expansion.cluster;
     }
   }
 }
 
-function regionQuery(set, point, eps) {
+function regionQuery(set, point_index, eps) {
   var neighbourPoints = [];
   for(var i=0;i<set.length;i++) {
-    if (pointDistance(point, set[i]) <= eps ) {
+    if(i===point_index) {
+      continue;
+    }
+    if (pointDistance(set[point_index], set[i]) <= eps ) {
       neighbourPoints.push(set[i]);
     }
   }
@@ -116,15 +151,15 @@ function expandCluster(point, neighbourPoints, cluster, eps, minPts, visited) {
       }
     }
   }
+  return {
+    visited_points: visited,
+    neighbour_points: neighbourPoints,
+    cluster: cluster
+  }
 }
 
 function pointDistance(point1, point2) {
-  var x1 = point1[0];
-  var y1 = point1[1];
-  var x2 = point2[0];
-  var y2 = point2[1];
-
-  var a = Math.abs(x1-x2);
-  var b = Math.abs(y1-y2);
+  var a = Math.abs(point1.x-point2.x);
+  var b = Math.abs(point1.y-point2.y);
   return Math.sqrt(a*a + b*b);
 }
