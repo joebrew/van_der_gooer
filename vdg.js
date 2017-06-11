00ff77967cbecf9bd248dae07b5fad2a27d70162
jerzyn <andrew.jarzyna@gmail.com>
2017-06-11 11:29:12 +0200

    DBSCAN library load

--- vdg.js
+++ vdg.js
@@ -1,7 +1,7 @@
 var dbscan_org = require('./dbscan.js');
 var fs = require('fs');
 
-var file = __dirname + '/data/testing_data_sets/data_set_1.json';
+var file = __dirname + '/data/testing_data_sets/data_set_1000.json';
 
 var data = JSON.parse(fs.readFileSync(file, 'utf-8'));
 console.log(dbscan_org(data, 100, 20));
