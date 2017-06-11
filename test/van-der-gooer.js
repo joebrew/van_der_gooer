bae873eb3771c14bebbfcaca26db20b5041ff98f
jerzyn <andrew.jarzyna@gmail.com>
2017-06-11 17:04:33 +0200

    Fix region query test to include the point itself

--- test/van-der-gooer.js
+++ test/van-der-gooer.js
@@ -2,31 +2,44 @@ const expect = require('chai').expect
 const should = require('chai').should()
 const vanDerGoer = require('../van-der-gooer')
 
-describe('Geographical Distance', function() {
-    it('is a finite number', function() {
-        expect(vanDerGoer.geographicalDistance({ "lng": 5.2515978518195, "lat": 13.1182789771101 }, { "lng": 5.24215150887217, "lat": 13.064493088185 })).to.be.finite
+describe('Density Based Clustering', function() {
+    describe('Geographical distances', function() {
+        it('is a finite number', function() {
+            expect(vanDerGoer.geographicalDistance({ "lng": 5.2515978518195, "lat": 13.1182789771101 }, { "lng": 5.24215150887217, "lat": 13.064493088185 })).to.be.finite
+        })
+        it('is a positive number', function() {
+            expect(vanDerGoer.geographicalDistance({ "lng": 5.2515978518195, "lat": 13.1182789771101 }, { "lng": 5.24215150887217, "lat": 13.064493088185 })).to.be.not.below(0)
+        })
     })
-    it('is a positive number', function() {
-        expect(vanDerGoer.geographicalDistance({ "lng": 5.2515978518195, "lat": 13.1182789771101 }, { "lng": 5.24215150887217, "lat": 13.064493088185 })).to.be.not.below(0)
+
+    describe('Neighbourhoods', () => {
+        let testSet = [{ lat: 41.3915104, lng: 2.1872686 }, { lat: 41.390872, lng: 2.185977 }, { lat: 41.391077, lng: 2.186229 }]
+
+        it('is an array', function() {
+            expect(vanDerGoer.regionQuery(testSet, 0, 200)).to.be.an('array')
+        })
+        it('is a correct set', function() {
+            expect(vanDerGoer.regionQuery(testSet, 0, 200)[1].lat).to.equal(41.390872) && expect(vanDerGoer.regionQuery(testSet, 0, 200)).to.have.lengthOf(3)
+        })
+        it('does not contain an incorrect set', function() {
+            expect(vanDerGoer.regionQuery(testSet, 0, 20)).to.have.lengthOf(1) && expect(vanDerGoer.regionQuery(testSet, 0, 20)[0].lat).to.equal(41.3915104)
+        })
+        it('should contain only elements from the given set', function() {
+            vanDerGoer.regionQuery(testSet, 0, 200)[0].should.be.oneOf(testSet).with.property('lat')
+        })
+    })
+
+    describe('Expanding the cluster', function() {
+
+    })
+
+    describe('DBSCAN clustering', function() {
+
     })
 })
 
-describe('Neighbourhoods', () => {
-    let testSet = [{ lat: 41.3915104, lng: 2.1872686 }, { lat: 41.390872, lng: 2.185977 }, { lat: 41.391077, lng: 2.186229 }]
 
-    it('are an array', function() {
-        expect(vanDerGoer.regionQuery(testSet, 0, 200)).to.be.an('array')
-    })
-    it('are a correct set', function() {
-        expect(vanDerGoer.regionQuery(testSet, 0, 200)[0].lat).to.equal(41.390872) && expect(vanDerGoer.regionQuery(testSet, 0, 200)).to.have.lengthOf(2)
-    })
-    it('do not contain an incorrect set', function() {
-        expect(vanDerGoer.regionQuery(testSet, 0, 20)).to.be.empty
-    })
-    it('should contain only elements given', function() {
-        vanDerGoer.regionQuery(testSet, 0, 200)[0].should.be.oneOf(testSet).with.property('lat')
-    })
-})
+
 
 
 // vanDerGoer.dbscan(
