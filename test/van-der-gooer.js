e85b119cc16203b4306adb38a748e96f98094977
jerzyn <andrew.jarzyna@gmail.com>
2017-06-11 14:55:22 +0200

    Add tests and fix dbscan expected values

---
+++ test/van-der-gooer.js
@@ -0,0 +1,37 @@
+const expect = require('chai').expect
+const vanDerGoer = require('../van-der-gooer')
+
+describe('Geographical Distance', function() {
+    it('is a finite number', function() {
+        expect(vanDerGoer.geographicalDistance({ "lng": 5.2515978518195, "lat": 13.1182789771101 }, { "lng": 5.24215150887217, "lat": 13.064493088185 })).to.be.finite
+    })
+    it('is a positive number', function() {
+        expect(vanDerGoer.geographicalDistance({ "lng": 5.2515978518195, "lat": 13.1182789771101 }, { "lng": 5.24215150887217, "lat": 13.064493088185 })).to.be.not.below(0)
+    })
+})
+
+describe('Neighbourhoods', () => {
+    let testSet = [{ lat: 41.3915104, lng: 2.1872686 }, { lat: 41.390872, lng: 2.185977 }, { lat: 41.391077, lng: 2.186229 }]
+
+    it('are an array', function() {
+        expect(vanDerGoer.regionQuery(testSet, 0, 200)).to.be.an('array')
+    })
+    it('are a correct set', function() {
+        expect(vanDerGoer.regionQuery(testSet, 0, 200)[0].lat).to.equal(41.390872)
+    })
+    it('do not contain an incorrect set', function() {
+        expect(vanDerGoer.regionQuery(testSet, 0, 20)).to.be.empty
+    })
+})
+
+
+// vanDerGoer.dbscan(
+//     [{ "x": 5.2515978518195, "y": 13.1182789771101, "true_label": "880", "lng": 5.2515978518195, "lat": 13.1182789771101, "label": "880", "correct": true, "optional": true },
+//         { "x": 5.24215150887217, "y": 13.064493088185, "true_label": "880", "lng": 5.24215150887217, "lat": 13.064493088185, "label": "880", "correct": true, "optional": true },
+//         { "x": 5.24517389783839, "y": 13.0807865667252, "true_label": "880", "lng": 5.24517389783839, "lat": 13.0807865667252, "label": "880", "correct": true, "optional": true },
+//         { "x": 5.20507742957126, "y": 13.0693288192475, "true_label": "880", "lng": 5.20507742957126, "lat": 13.0693288192475, "label": "880", "correct": true, "optional": true },
+//         { "x": 5.26150717656308, "y": 13.0069578754319, "true_label": "880", "lng": 5.26150717656308, "lat": 13.0069578754319, "label": "880", "correct": true, "optional": true }
+//     ],
+//     100,
+//     2
+// )
--- van-der-gooer.js
+++ van-der-gooer.js
@@ -1,7 +1,7 @@
 // ---------------------------------- LOADING DATA ------------------------------------------------------------------
 
-var json = require('./testing_data_sets/data_set_1.json');
-var dataset = JSON.parse(json);
+//var json = require('./testing_data_sets/data_set_1.json');
+//var dataset = JSON.parse(json);
 // d3.csv("./data_set_1.csv", function(data) {
 //   debugger;
 //   console.log(data);
@@ -24,34 +24,99 @@ var dataset = JSON.parse(json);
 // We assume that the structure of the set is as follows:
 // var set = [
 //   {
-//     x: value,
-//     y: value,
+//     lat: value,
+//     lng: value,
 //     label: value,
 //     true_label: value
 //   }
 // ]
-function dbscan(set, eps, minPts) {
-    var c = [];
-    var visited = [];
-    var noise = [];
-    // var dPrime = d;
-    for (var i = 0; i < set.length; i++) {
-        var neighbourPoints = [];
-        visited.push(set[i]);
-        neighbourPoints.concat(regionQuery(set, i, eps)); // review this code
-        if (neighbourPoints.length < minPts) {
-            noise.push(set[i]);
-        } else {
-            var clusterId = c.length;
-            c.push([]);
-            c[clusterId].push(set[i]);
-            var expansion = expandCluster(set[i], neighbourPoints, c[clusterId], eps, minPts, visited);
-            visited = expansion.visited_points;
-            neighbourPoints = expansion.neighbour_points;
-            c = expansion.cluster;
+
+module.exports = {
+    dbscan: function(set, eps, minPts) {
+        var c = [];
+        var visited = [];
+        var noise = [];
+        // var dPrime = d;
+        for (var i = 0; i < set.length; i++) {
+            var neighbourPoints = [];
+            visited.push(set[i]);
+            neighbourPoints.concat(regionQuery(set, i, eps));
+            if (neighbourPoints.length < minPts) {
+                noise.push(set[i]);
+            } else {
+                var clusterId = c.length;
+                c.push([]);
+                c[clusterId].push(set[i]);
+                var expansion = expandCluster(set[i], neighbourPoints, c[clusterId], eps, minPts, visited);
+                visited = expansion.visited_points;
+                neighbourPoints = expansion.neighbour_points;
+                c = expansion.cluster;
+            }
         }
+        console.log(c);
+    },
+
+    regionQuery: function(set, point_index, eps) {
+        var neighbourPoints = [];
+        for (var i = 0; i < set.length; i++) {
+            if (i === point_index) {
+                continue;
+            }
+            if (geographicalDistance(set[point_index], set[i]) <= eps) {
+                neighbourPoints.push(set[i]);
+            }
+        }
+        return neighbourPoints;
+    },
+
+    expandCluster: function(point, neighbourPoints, cluster, eps, minPts, visited) {
+        // var visited = [];
+        cluster.push(point);
+        var neighbourPointsPrime = [];
+        for (var i = 0; i < neighbourPoints.length; i++) {
+            if (!visited.find(neighbourPoints[i])) { //if find does not work like this, try with a callback function
+                visited.push(neighbourPoints[i]);
+                neighbourPointsPrime = regionQuery(neighbourPoints, neighbourPoints[i], eps);
+                if (neighbourPointsPrime.length >= minPts) {
+                    neighbourPoints.concat(neighbourPointsPrime);
+                }
+                if (!cluster.find(neighbourPoints[i])) {
+                    cluster.push(neighbourPoints[i]);
+                }
+            }
+        }
+        return {
+            visited_points: visited,
+            neighbour_points: neighbourPoints,
+            cluster: cluster
+        }
+    },
+
+    pointDistance: function(point1, point2) {
+        var a = Math.abs(point1.x - point2.x);
+        var b = Math.abs(point1.y - point2.y);
+        return Math.sqrt(a * a + b * b);
+    },
+
+    geographicalDistance: function(point1, point2) {
+        var Rk = 6373;
+        var lat1 = deg2rad(point1.lat);
+        var lon1 = deg2rad(point1.lng);
+        var lat2 = deg2rad(point2.lat);
+        var lon2 = deg2rad(point2.lng);
+
+        var dlat = lat2 - lat1;
+        var dlon = lon2 - lon1;
+
+        var a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)); // great circle distance in radians
+        return c * Rk * 1000; // great circle distance in km
+    },
+
+    deg2rad: function(deg) {
+        rad = deg * Math.PI / 180; // radians = degrees * pi/180
+        return rad;
     }
-    console.log(c);
 }
 
 function regionQuery(set, point_index, eps) {
@@ -108,7 +173,7 @@ function geographicalDistance(point1, point2) {
 
     var a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a)); // great circle distance in radians
-    return c * Rk; // great circle distance in km
+    return c * Rk * 1000; // great circle distance in m
 }
 
 function deg2rad(deg) {
