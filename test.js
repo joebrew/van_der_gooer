587e0757ce31d416344798080a5ec0ac0fee8670
jerzyn <andrew.jarzyna@gmail.com>
2017-06-11 15:30:09 +0200

    Manual testing file for reference

---
+++ test.js
@@ -0,0 +1,4 @@
+const vdg = require('./van-der-gooer')
+
+
+console.log(vdg.regionQuery([{ lat: 41.3915104, lng: 2.1872686 }, { lat: 41.390872, lng: 2.185977 }, { lat: 41.391077, lng: 2.186229 }], 1, 200000))
