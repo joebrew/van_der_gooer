c3ac90ad6c65cb347f925266a9630c5909257599
jerzyn <andrew.jarzyna@gmail.com>
2017-06-13 17:44:31 +0200

    Add DBSCAN tests and lodash

--- test/van-der-gooer.js
+++ test/van-der-gooer.js
@@ -1,5 +1,6 @@
 const expect = require('chai').expect
 const should = require('chai').should()
+const _ = require('lodash')
 const vanDerGoer = require('../van-der-gooer')
 
 describe('Density Based Clustering', function() {
@@ -19,22 +20,44 @@ describe('Density Based Clustering', function() {
             expect(vanDerGoer.regionQuery(testSet, 0, 200)).to.be.an('array')
         })
         it('is a correct set', function() {
-            expect(vanDerGoer.regionQuery(testSet, 0, 200)[1].lat).to.equal(41.390872) && expect(vanDerGoer.regionQuery(testSet, 0, 200)).to.have.lengthOf(3)
+            expect(vanDerGoer.regionQuery(testSet, 0, 200)[0].lat).to.equal(41.390872) && expect(vanDerGoer.regionQuery(testSet, 0, 200)).to.have.lengthOf(2)
         })
         it('does not contain an incorrect set', function() {
-            expect(vanDerGoer.regionQuery(testSet, 0, 20)).to.have.lengthOf(1) && expect(vanDerGoer.regionQuery(testSet, 0, 20)[0].lat).to.equal(41.3915104)
+            expect(vanDerGoer.regionQuery(testSet, 0, 20)).to.be.empty
         })
         it('should contain only elements from the given set', function() {
             vanDerGoer.regionQuery(testSet, 0, 200)[0].should.be.oneOf(testSet).with.property('lat')
         })
     })
 
-    describe('Expanding the cluster', function() {
-
-    })
-
     describe('DBSCAN clustering', function() {
+        let testSet = [{ lat: 41.3915104, lng: 2.1872686 }, { lat: 41.390872, lng: 2.185977 }, { lat: 41.391077, lng: 2.186229 }, { "lat": 5.25398996489777, "lng": 13.1197878250267 }, { lat: 5.25074004673171, lng: 13.1219781282893 }]
 
+        it('returns a set of clusters', function() {
+            expect(vanDerGoer.dbscan(testSet, 3, 250).clusters).to.be.an('array')
+        })
+
+        it('each cluster is a set of points', function() {
+            expect(vanDerGoer.dbscan(testSet, 3, 250).clusters[0]).to.be.an('array')
+        })
+
+        it('returns set of noise points', function() {
+            expect(vanDerGoer.dbscan(testSet, 3, 250).noise).to.be.an('array')
+        })
+
+        it('returns unique values', function() {
+            _.intersection(vanDerGoer.dbscan(testSet, 3, 250), vanDerGoer.dbscan(testSet, 3, 250)).should.be.empty
+        })
+
+        it('returns a correct number of clusters', function() {
+            vanDerGoer.dbscan(testSet, 2, 500).clusters.length.should.equal(2)
+        })
+
+        it('returns sets of correct size', function() {
+            vanDerGoer.dbscan(testSet, 2, 500).clusters[0].length.should.equal(3)
+            vanDerGoer.dbscan(testSet, 2, 500).clusters[1].length.should.equal(2)
+            vanDerGoer.dbscan(testSet, 2, 250).noise.length.should.equal(2)
+        })
     })
 })
 
